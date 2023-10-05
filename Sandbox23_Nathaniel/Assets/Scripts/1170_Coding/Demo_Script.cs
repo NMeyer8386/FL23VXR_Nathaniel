d80eb6b59ed0f60ea055693c8a850cf3931fd343
NMeyer8386 <90421439+NMeyer8386@users.noreply.github.com>
2023-10-05 09:17:07 -0600

    changed the name of the demo scene and did documentation of code

--- Sandbox23_Nathaniel/Assets/Scripts/1170_Coding/Demo_Script.cs
+++ Sandbox23_Nathaniel/Assets/Scripts/1170_Coding/Demo_Script.cs
@@ -6,24 +6,32 @@ using TMPro;
 using System;
 public class Demo_Script : MonoBehaviour
 {
+    //Variable Declarations
     [Header("Thing")]
     public GameObject thing;
+
     [Header("Sliders")]
     public Slider sliderX;
     public Slider sliderY;
     public Slider sliderZ;
+
     [Header("Labels")]
     public TMP_Text labelX;
     public TMP_Text labelY;
     public TMP_Text labelZ;
+
+    //Function that's called every time the slider value changes
     public void ChangeValue()
     {
+        //Create new Quaternion based on slider values
         Quaternion demoQuaternion = new Quaternion(sliderX.value, sliderY.value, sliderZ.value, 1);
 
+        //Change the labels to the slider value (rounded to 2 decimal points)
         labelX.text = Math.Round(sliderX.value, 2).ToString();
         labelY.text = Math.Round(sliderY.value, 2).ToString();
         labelZ.text = Math.Round(sliderZ.value, 2).ToString();
 
+        //Change the rotation to the Quaternion value
         thing.transform.rotation = demoQuaternion;
     }
 }
